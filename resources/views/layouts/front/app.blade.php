38fbda717be7125d5f58142645cf3ec1d2809964
nada soultan <nada.ebrahim.soultan@gmail.com>
2022-05-31 11:18:56 +0200

    nada

--- resources/views/layouts/front/app.blade.php
+++ resources/views/layouts/front/app.blade.php
@@ -1,30 +1,34 @@
 <!DOCTYPE html>
 <html lang="en-US" dir="ltr">
 
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- 11    <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
+
 
 
     <!-- ===============================================-->
     <!--    Document Title-->
     <!-- ===============================================-->
-    <title>Falcon | Dashboard &amp; Web App Template</title>
+    <title>Sonoo - Affiliate Platform</title>
+
+
 
 
     <!-- ===============================================-->
     <!--    Favicons-->
     <!-- ===============================================-->
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/assets/img/favicons/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/assets/img/favicons/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/assets/img/favicons/favicon-16x16.png') }}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('/assets/img/favicons/favicon.ico') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/fevicon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/fevicon.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/fevicon.png') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/fevicon.png') }}">
     <link rel="manifest" href="{{ asset('/assets/img/favicons/manifest.json') }}">
-    <meta name="msapplication-TileImage" content="{{ asset('/assets/img/favicons/mstile-150x150.png') }}">
+    <meta name="msapplication-TileImage" content="{{ asset('assets/img/fevicon.png') }}">
     <meta name="theme-color" content="#ffffff">
-    <script src="{{ asset('/assets/js/config.js') }}"></script>
-    <script src="{{ asset('/vendors/overlayscrollbars/OverlayScrollbars.min.js') }}"></script>
+    <script src="{{ asset('assets/js/config.js') }}"></script>
+    <script src="{{ asset('vendors/overlayscrollbars/OverlayScrollbars.min.js') }}"></script>
 
 
     <!-- ===============================================-->
@@ -38,7 +42,7 @@
     <link href="{{ asset('/assets/css/theme.min.css') }}" rel="stylesheet" id="style-default">
     <link href="{{ asset('/assets/css/user-rtl.min.css') }}" rel="stylesheet" id="user-style-rtl">
     <link href="{{ asset('/assets/css/user.min.css') }}" rel="stylesheet" id="user-style-default">
-    <link href="{{ asset('/assets/css/theme.css') }}" rel="stylesheet">
+    <link href="{{ asset('/assets/css/custom.css') }}" rel="stylesheet">
     <script>
       var isRTL = JSON.parse(localStorage.getItem('isRTL'));
       if (isRTL) {
@@ -58,6 +62,7 @@
 
 
   <body>
+
       @include('layouts.front._header')
 
       @yield('content_front')
@@ -83,3 +88,5 @@
     </body>
   
   </html>
+    
+  
