4f43deebbee2f13337696905369ace6ba5c2b93f
nada soultan <nada.ebrahim.soultan@gmail.com>
2022-06-20 17:37:57 +0200

    last2

--- resources/views/layouts/front/_whats.blade.php
+++ resources/views/layouts/front/_whats.blade.php
@@ -1,7 +1,7 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <div>
-                <a href="https://wa.me/01012555108" class="float" target="_blank">
+                <a href="https://wa.link/00apk4" class="float" target="_blank">
                <i class=" fab fa-whatsapp my-float"></i>
                 </a>
     
